8474fd80359bea07dcfaf621abcf27141ec07fc9
Jeremy Hinegardner <jeremy@hinegardner.org>
2011-08-05 22:49:49 -0400

    Tomdocify location_of_caller

--- lib/deprecatable/util.rb
+++ lib/deprecatable/util.rb
@@ -1,7 +1,18 @@
 module Deprecatable
   # Common utility functions used by all the Deprecatable modules and classes
   module Util
-    # The file name and line number of the caller of the caller of this method.
+    # Find the caller of the method that called the method where
+    # location_of_call was invoked. In other words:
+    #
+    #     def foo
+    #       bar()  # <--- this file and line number is returned
+    #     end
+    #
+    #     def bar
+    #       Deprecatable::Util.location_of_caller
+    #     end
+    #
+    # return the file and line number from which bar() was invoked.
     def self.location_of_caller
       call_line     = caller[1]
       file, line, _ = call_line.split(':')
